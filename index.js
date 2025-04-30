61d24f30ed3727fb49c04308b35619c7123c20b1
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-04-29 22:30:38 -0600

    domcontentloaded

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
-  });
-
-  console.log(
-    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-  );
-
-  document.getElementById("textcontent").textContent = "This is really cool!";
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("textContent").textContent = "This is really cool!";
+});
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
